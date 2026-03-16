3cd7161427e185a063510ef7bc5db0f37ae4e1c8
KarthickRamAlagar <karthickramalagar@gmail.com>
2026-03-17 03:02:01 +0530

    update subjects list and constants

--- src/App.tsx
+++ src/App.tsx
@@ -13,10 +13,12 @@ import { useNotificationProvider } from "./components/refine-ui/notification/use
 import { ThemeProvider } from "./components/refine-ui/theme/theme-provider";
 import { dataProvider } from "./providers/data.ts";
 import DashBoard from "./pages/dashboard";
-import { BookOpen, GraduationCap, Home } from "lucide-react";
+import { BookOpen, GraduationCap, GraduationCapIcon, Home } from "lucide-react";
 import { Layout } from "./components/refine-ui/layout/layout";
 import SubjectList from "./pages/subjects/list";
 import SubjectCreate from "./pages/subjects/create";
+import ClassesList from "./pages/classes/list";
+import ClassesCreate from "./pages/classes/create";
 
 function App() {
   return (
@@ -55,6 +57,15 @@ function App() {
                     icon: <BookOpen />,
                   },
                 },
+                {
+                  name: "classes",
+                  list: "/classes",
+                  create:'/classes/create',
+                  meta: {
+                    label: "Classes",
+                    icon: <GraduationCapIcon />,
+                  },
+                },
               ]}
             >
               <Routes>
@@ -69,6 +80,10 @@ function App() {
                   <Route path="subjects">
                       <Route index element={<SubjectList/>} />
                       <Route path='create' element={<SubjectCreate/>} />
+                  </Route>
+                  <Route path="classes">
+                      <Route index element={<ClassesList/>} />
+                      <Route path='create' element={<ClassesCreate/>} />
                   </Route>
                 </Route>
               </Routes>
